b9be4170fefa59e5d9dc8a9b8ffdf6f1134aa278
keith <keithh@lakeview.co.uk>
2019-01-10 00:32:14 +0000

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBnB4GMcZ4lKs-SpDjoG6_4iPgLMFPOwyU',
+    authDomain: 'flex-test-83bd9.firebaseapp.com',
+    databaseURL: 'https://flex-test-83bd9.firebaseio.com',
+    projectId: 'flex-test-83bd9',
+    storageBucket: 'flex-test-83bd9.appspot.com',
+    messagingSenderId: '934164892077'
+  }
 };
